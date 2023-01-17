d391f328bda22e3e72d8322943d6e6ec38b02dcf
SweetLejo <heldal.leonard@gmail.com>
2023-01-17 15:17:14 +0100

    filmarkiv interface and array class

---
+++ kode/src/dat102/adt/FilmArkiv.java
@@ -0,0 +1,90 @@
+package dat102.adt;
+
+import dat102.Film;
+import dat102.Sjanger;
+
+public class FilmArkiv implements FilmArkivADT {
+
+    private Film[] movieList;
+    private int nextFree;
+
+    public FilmArkiv(){
+
+    }
+    public FilmArkiv(int nMovies){
+        this.movieList = new Film[nMovies];
+        this.nextFree = 0;
+    }
+
+    private void extend(){
+        Film[] newMovieList = new Film[movieList.length * 2];
+        System.arraycopy(newMovieList, 0, movieList, 0, movieList.length);
+        this.movieList = newMovieList;
+    }
+
+
+
+    @Override
+    public Film finnFilm(int nr) {
+        boolean funnet = false;
+        Film match = null;
+        for(int i = 0; i < nextFree && !funnet; i ++){
+            funnet = movieList[i].getMovieNum() == nr;
+            match = funnet? movieList[i] : null;
+        }
+
+        return match;
+    }
+
+    @Override
+    public void addMovie(Film newMovie) {
+        if(movieList[movieList.length-1] != null) extend();
+        movieList[nextFree] = newMovie;
+        this.nextFree++;
+    }
+
+    @Override
+    public boolean removeMovie(int movienr) {
+        boolean match = false;
+        Film movie = finnFilm(movienr);
+        for(int i = 0 ; i < nextFree && !match ; i++){
+            match = movieList[i].equals(movie);
+            if(match) {
+                movieList[i] = movieList[nextFree-1];
+                nextFree--;
+            }
+        }
+        return match;
+    }
+
+    @Override
+    public Film[] searchTitle(String keyWord) {
+        Film[] matchList = new Film[nextFree];
+        int matches = 0;
+        for(int i = 0 ; i < nextFree ; i++){
+            if(movieList[i].getTitle().contains(keyWord)){
+                matchList[matches] = movieList[i];
+                matches++;
+            }
+        }
+
+        Film[] newMatchList = new Film[matches];
+        System.arraycopy(newMatchList, 0, matchList, 0, matches);
+        return newMatchList;
+    }
+
+    @Override
+    public int numInGenre(Sjanger genre) {
+        int numInGenre = 0;
+        for(Film f : movieList){
+            if(f.getGenre()== genre) numInGenre++;
+        }
+
+        return numInGenre;
+    }
+
+    @Override
+    public int amount() {
+        return nextFree;
+    }
+}
---
+++ kode/src/dat102/adt/FilmArkivADT.java
@@ -0,0 +1,50 @@
+package dat102.adt;
+
+import dat102.Film;
+import dat102.Sjanger;
+public interface FilmArkivADT {
+
+
+    /**
+     * Gets a movie given the number
+     * @param nr number of the movie
+     * @return movie object with the nr, if no match then null.
+     */
+   Film finnFilm(int nr);
+
+    /**
+     * Adds a movie
+     * @param newMovie The movie you want to add
+     */
+   void addMovie(Film newMovie);
+
+    /**
+     * removes movie given the movie num
+     * @param movie the number of the movie
+     * @return true if removed, if not removed false
+     */
+   boolean removeMovie(int movie);
+
+
+    /**
+     * Searches for movies given a word
+     * @param keyWord the word that has to be in the title
+     * @return array of movies with the given title
+     */
+   Film[] searchTitle(String keyWord);
+
+
+    /**
+     * number of movies in given genre
+     * @param genre enum Sjanger (genre)
+     * @return number of movies in genre
+     */
+   int numInGenre(Sjanger genre);
+
+    /**
+     *
+     * @return Number of movies in archive
+     */
+   int amount();
+
+}
