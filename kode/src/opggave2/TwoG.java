0ff04557b39ddf82b8befff6a4222a8407272916
SweetLejo <heldal.leonard@gmail.com>
2023-01-24 13:57:53 +0100

    finished question 2 with class and text file

--- kode/src/opggave2/TwoG.java
+++ kode/src/opggave2/TwoG.java
@@ -2,12 +2,29 @@ package opggave2;
 
 public class TwoG {
 
-    public static void tid(long n) {
-        // ...fyll ut
+    public static void main(String[] args){
+
+        snitt(10_000_000);
+        snitt(100_000_000);
+        snitt(1_000_000_000);
+    }
+
+    public static void snitt(long n){
+        long sum = 0;
+        for(int i = 0 ; i < 200 ; i++){
+            sum += tid(n);
+        }
+        System.out.println(sum/10.0);
+    }
+
+
+
+    public static long tid(long n) {
+        long start = System.currentTimeMillis();
         long k = 0;
         for (long i = 1; i <= n; i++) {
             k = k + 5;
         }
-        // ...fyll ut
+        return (System.currentTimeMillis() - start);
     }
 }
